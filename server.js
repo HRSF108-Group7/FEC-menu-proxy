541e888a4a54714932c1cd590c791acd7a90ddcb
DymaxionMap <DymaxionMap@users.noreply.github.com>
2019-01-03 15:02:43 -0800

    Add Profile service

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ app.get('/restaurants/:id', (req, res ) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/restaurants/:id/profile', (req, res) => {
+  const id = req.params.id;
+  axios.get(`http://localhost:3001/restaurants/${id}/profile`)
+    .then(response => response.data)
+    .then(data => res.send(data))
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+})
+
 app.get('/restaurants/:id/menu-items', (req, res) => {
   const id = req.params.id;
   axios.get(`http://localhost:3002/restaurants/${id}/menu-items`)
@@ -27,7 +38,7 @@ app.get('/restaurants/:id/suggestions', (req, res) => {
       console.error(err);
       res.sendStatus(500);
     });
-})
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
