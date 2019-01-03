703eb2808718f6a46223acd42bedc3b154d29203
DymaxionMap <DymaxionMap@users.noreply.github.com>
2019-01-03 11:45:15 -0800

    Add Menu service

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 
 const app = express();
 app.get('/restaurants/:id', (req, res ) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/restaurants/:id/menu-items', (req, res) => {
+  const id = req.params.id;
+  axios.get(`http://localhost:3002/restaurants/${id}/menu-items`)
+    .then(response => response.data)
+    .then(data => res.send(data))
+    .catch(err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
 
 const PORT = 3000;
